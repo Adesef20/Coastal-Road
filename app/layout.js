0d62ae3d6862475a221276a9734b4db513c06753
Adesef20 <musefiuadebisi@gmail.com>
2025-09-22 11:42:33 +0100

    Fresh start: fix imports and clean setup

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css';
-import Navbar from '@/components/Navbar';
-import Footer from '@/Components/Footer';
+import Navbar from '../components/Navbar';
+import Footer from '../Components/Footer';
 
 
 
